c219a228ebea563b62fce7f3cb45cef2fefc2fe7
Sergio Campos <sergio.camposj@gmail.com>
2018-02-03 15:58:20 -0600

    EvaluacionCurosXamarinTowa

---
+++ EvaluacionFinalXF/EvaluacionFinalXF/EvaluacionFinalXF/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace EvaluacionFinalXF
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			//MainPage = new EvaluacionFinalXF.MainPage();
+            MainPage = new NavigationPage(new pageLogin());
+           
+
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
